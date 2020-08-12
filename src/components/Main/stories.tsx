01173f117e8daf666d8662b061d9f91113a3361b
luizeboli <unifelicio@gmail.com>
2020-08-12 13:02:37 -0300

    refactor: storybook v6 breaking changes

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,14 +1,15 @@
 import { text, withKnobs } from '@storybook/addon-knobs';
+import { Meta, Story } from '@storybook/react/types-6-0';
 
-import Main from './';
+import Main from '.';
 
 export default {
   title: 'Main',
   component: Main,
   decorators: [withKnobs],
-};
+} as Meta;
 
-export const Basic = () => (
+export const Basic: Story = () => (
   <Main
     title={text('Title', 'Boilerplate')}
     description={text(
